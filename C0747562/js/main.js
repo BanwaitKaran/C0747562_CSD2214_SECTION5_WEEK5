34f3c343c3b5cefa5fa13d6c2fec48b68b62909d
KARAN BANWAIT <karanbanwaitsaini@gmail.com>
2019-02-07 11:44:39 -0500

    commiting

--- C0747562/js/main.js
+++ C0747562/js/main.js
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿function myFunction(a, b) {
+   // var a = 5;
+   // var b = 6;
+    return a + b; //The function returns the sum of a and b
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "green", "orange", "blue", "purple"];
+    return pens;
+}
+
